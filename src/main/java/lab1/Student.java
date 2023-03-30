d897b72e3a687d549fce103458ee8a65e86c488e
JerryFlop <JParedes@wil.yearup.org>
2023-03-29 23:55:51 -0400

    new commit

--- src/main/java/lab1/Student.java
+++ src/main/java/lab1/Student.java
@@ -2,5 +2,11 @@ package lab1;
 
 public class Student {
  public String name;
+ public int age;
+ public char grade;
+
+ public void printStudentinfo(){
+  System.out.println("Student Name:" + this.name + " " + "Age:" + this.age + " " + " class grade" + this.grade);
+ }
 
 }
---
+++ src/main/java/lab1/StudentInfo.java
@@ -0,0 +1,19 @@
+package lab1;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        one.age = 5
+
+        Student student2 = new Student();
+
+
+
+
+
+
+
+
+
+    }
+}
